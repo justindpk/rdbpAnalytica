8faa2929bfda3bea342919bde3028630f94ef0f1
Christian Piscos <christian.piscos@gmail.com>
2022-09-01 22:45:55 -0700

    Data parsing clean up

--- main.js
+++ main.js
@@ -5,16 +5,21 @@ async function getDataFromUrl(url) {
     return data.data
 }
 
-function read_data(data) {
-    console.log(JSON.parse(data))
+function parseAllDucks(name, data) {
+    data = data.replace(name + ' = ', '')
+    data = JSON.parse(data)
+    console.log(data)
+    return data
 }
 
 URLS = {
     'allDucks': 'https://duck.art/rarity-data/v7/allDucks.js',
     'globalRarity': 'https://duck.art/rarity-data/v7/globalRarity.js',
-    'allBackpacks': 'https://duck.art/rarity-data/v7/globalRarity.js',
-    'backpackRarity': 'https://duck.art/rarity-data/v7/globalRarity.js',
+    'allBackpacks': 'https://duck.art/rarity-data/v7/allBackpacks.js',
+    'backpackRarity': 'https://duck.art/rarity-data/v7/backpackRarity.js',
 }
 
-//Test func
-getDataFromUrl(URLS.allDucks).then(data => read_data(data))
+
+for (const [database_name, database_url] of Object.entries(URLS)) {
+    getDataFromUrl(database_url).then(data => { parseAllDucks(database_name, data)})
+}
