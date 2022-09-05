a63e9189399955b05c244f9c9800f4aaf4e834d4
Christian Piscos <christian.piscos@gmail.com>
2022-09-05 12:58:51 -0700

    Made table generation more modular

--- client/src/ducktable.js
+++ client/src/ducktable.js
@@ -3,92 +3,91 @@ const amountToLoad = 50;
 let counter = 0;
 
 window.onload = () => {
+    loadTableHeader(homeConfig);
     fetch('http://localhost:3005/api').then(res => res.json()).then(data => {
-        loadTableData(data, 0, initialLoad);
+        loadTableData(data, 0, initialLoad, homeConfig);
     });
 };
-window.onscroll = function (ev) {
+window.onscroll = function () {
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight * 0.95) {
         console.log("loading more ducks");
         fetch('http://localhost:3005/api').then(res => res.json()).then(data => {
             loadTableData(data,
                 initialLoad + counter * amountToLoad,
-                initialLoad + (counter + 1) * amountToLoad);
+                initialLoad + (counter + 1) * amountToLoad,
+                homeConfig);
             counter++;
         });
 
     }
 };
 
-
-    function loadTableData(duckData, start, stop) {
-        // Potential modular function
-        // function generateTable(array) {
-        //     duckRow = `<tr>`
-        //     for (let i = 0; i < array.length; i++) {
-        //         if (array[i].includes('.png')) {
-        //             duckRow += `<td><img className="duckImage" src="${array[i]}"></td>    `;
-        //         } else {
-        //             duckRow += `<td><strong class="rowTitle">${array[i]}</strong></td>`;
-        //         }
-        //         duckRow += `<td>
-        //             <a href="https://opensea.io/collection/rubber-duck-bath-party" class="rowTitle"><img src="/client/public/img/opensea.svg"></a>
-        //             </td>`
-        //         duckRow += `</tr>`;
-        //         return duckRow;
-        //     }
-        //     generateTable(["rank", "img", "id", "version", "parties", "rarityChange"]);
-        console.log(start, stop)
-        // console.log(duckData);
-        const tableBody = document.getElementById('tableData');
-        let duckRow = '';
-        let ducks = [];
-        for (let i = start; i < stop; i++) {
-            ducks.push(duckData[i]);
-        }
-        console.log(ducks.length);
-        /*
-            id rank version rarityChange img parties
-        */
-        for (let duck of ducks) {
-            duckRow += `<tr>
-                    <td>
-                        <strong class="rowTitle">${duck.rank}</strong>
-                    </td>
-                    <td>
-                        <img class="duckImage" src="${duck.img}">
-                    </td>
-                    <td>
-                    <strong class="rowTitle">${duck.id}</strong>
-                    </td>
-                    <td>
-                    <strong class="rowTitle">${duck.version}</strong>
-                    </td>
-                    <td>
-                    <strong class="rowTitle">${duck.parties}</strong>
-                    </td>
-                    <td>
-                    <strong class="rowTitle">${duck.rarityChange}</strong>
-                    </td>
-                    <td>
-                    <strong class="rowTitle">bludmoneyy</strong>
-                    </td>
-                    <td>
-                    <strong class="rowTitle">184</strong>
-                    </td>
-                    <td>
-                    <a href="https://opensea.io/collection/rubber-duck-bath-party" class="rowTitle"><img src="/client/public/img/opensea.svg"></a>
-                    </td>
-                    <td>
-                    <a href="https://opensea.io/collection/rubber-duck-bath-party" class="rowTitle"><img src="/client/public/img/looksrare.svg"></a>
-                    </td>
-                    </tr>`;
-            // console.log(tableBody.innerHTML, typeof tableBody.innerHTML);
-        }
-        if (start === 0) {
-            tableBody.innerHTML = duckRow;
-        } else {
-            tableBody.innerHTML += duckRow;
-            // tableBody.insertAdjacentHTML("afterend", duckRow);
-        }
+function columnConfig(columnName=null, duckKey=null, rowOverride=false) {
+    return {
+        headerName: columnName,
+        key: duckKey,
+        override: rowOverride
     }
+}
+
+const homeConfig = [
+    columnConfig("Rank", "rank"),
+    columnConfig(`<img class="duckIcon" src="/client/public/img/duckIcon.svg" alt="">`, "img"),
+    columnConfig("Number", "id"),
+    columnConfig("Version", "version"),
+    columnConfig("Parties", "parties"),
+    columnConfig("Rank Change", "rarityChange"),
+    columnConfig("Owner", "bludmoneyy", true),
+    columnConfig("Days Owned", "184", true),
+    columnConfig("Opensea", `<img src="/client/public/img/opensea.svg" alt="">`, true),
+    columnConfig("Looksrare", `<img src="/client/public/img/looksrare.svg" alt="">`, true),
+]
+
+const imageSuffixes = ['.png', '.jpg', '.jpeg', '.gif', '.svg'];
+
+function loadTableHeader(config) {
+    const tableHeader = document.getElementById('tableHeader');
+    let header = '<tr>';
+    for (let column of config) {
+        header += `<th scope="col">${column.headerName}</th>`;
+    }
+    header += '</tr>';
+    tableHeader.innerHTML = header;
+}
+
+function loadTableData(duckData, start, stop, config) {
+    const tableBody = document.getElementById('tableData');
+    let ducks = [];
+    for (let i = start; i < stop; i++) {
+        ducks.push(duckData[i]);
+    }
+    let duckTable = "";
+    for (let duck of ducks) {
+        let duckRow = '<tr>';
+        for (let column of config) {
+            if (column.override) {
+                if (column.key.includes(".img") || column.key.includes(".svg")) {
+                    duckRow += `<td><a href="https://opensea.io/collection/rubber-duck-bath-party" class="rowTitle">
+                                ${column.key}
+                                </a></td>`;
+                } else {
+                    console.log("rowOverride", column.override);
+                    duckRow += `<td><strong class="rowTitle">${column.key}</strong></td>`;
+                }
+
+            } else if (duck.hasOwnProperty(column.key) && imageSuffixes.some(suffix => String(duck[column.key]).endsWith(suffix))) {
+                duckRow += `<td><img class="duckIcon" src="${duck[column.key]}" alt=""></td>`;
+            } else {
+                duckRow += `<td><strong class="rowTitle">${duck[column.key]}</strong></td>`;
+            }
+        }
+        duckRow += '</tr>';
+        console.log(duckRow);
+        duckTable += duckRow;
+    }
+    if (start === 0) {
+        tableBody.innerHTML = duckTable;
+    } else {
+        tableBody.innerHTML += duckTable;
+    }
+}
