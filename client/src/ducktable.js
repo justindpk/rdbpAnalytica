38e23e0636ad115b878b4656d650efe338d03b9f
Christian Piscos <christian.piscos@gmail.com>
2022-09-06 00:51:55 -0700

    Updated table logic

--- client/src/ducktable.js
+++ client/src/ducktable.js
@@ -22,34 +22,125 @@ window.onscroll = function () {
     }
 };
 
-function columnConfig(columnName=null, duckKey=null, rowOverride=false) {
-    return {
-        headerName: columnName,
-        key: duckKey,
-        override: rowOverride
+class ColumnConfig {
+    constructor() {
+        this.headerName = null;
+        this.columnKey = null;
+        this.headerType = null;
+        this.headerSrc = null;
+        this.headerClass = null;
+        this.staticColumn = null;
+        this.columnType = 'strong';
+        this.columnSrc = null;
+        this.columnHref = null;
+        this.columnClass = null;
+    }
+
+    setHeaderType(headerType) {
+        this.headerType = headerType;
+        return this;
+    }
+    setHeaderStatic(headerName) {
+        this.headerName = headerName;
+        return this;
+    }
+    setHeaderSrc(src) {
+        this.headerSrc = src;
+        return this;
+    }
+    setHeaderClass(headerClass) {
+        this.headerClass = headerClass;
+        return this;
+    }
+    setColumnKey(key) {
+        this.columnKey = key;
+        return this;
+    }
+    setColumnStatic(value) {
+        this.staticColumn = value;
+        return this;
+    }
+    setColumnType(type) {
+        this.columnType = type;
+        return this;
+    }
+    setColumnSrc(src) {
+        this.columnSrc = src;
+        return this;
+    }
+    setColumnHref(href) {
+        this.columnHref = href;
+        return this;
+    }
+    setColumnClass(columnClass) {
+        this.columnClass = columnClass;
+        return this;
+    }
+    getHeaderTag() {
+        let tag = `<th scope="col">`
+        if (this.headerName) {
+            tag += `${this.headerName}`
+        } else {
+            tag += `<${this.headerType} `
+            if (this.headerClass) {
+                tag += `class="${this.headerClass}" `
+            }
+            if (this.headerSrc) {
+                tag += `src="${this.headerSrc}" `
+            }
+            tag += `>`
+        }
+        tag += `</th>`
+        return tag;
+
+    }
+    getColumnTag(duckData) {
+        let tag = `<td>`
+        if (this.columnHref) {
+            tag += `<a href="${this.columnHref}">`
+        }
+        tag += `<${this.columnType} `
+        if (this.columnClass) {
+            tag += `class="${this.columnClass}"`
+        }
+        if (this.staticColumn || this.columnKey && this.columnType !== 'img') {
+            tag += `>${this.staticColumn ? this.staticColumn : duckData[this.columnKey]}</${this.columnType}>`
+        } else if (this.columnSrc || this.columnType === 'img') {
+            tag += `src="${this.columnSrc ? this.columnSrc : duckData[this.columnKey]}">`
+        }
+        if (this.columnHref) {
+            tag += `</a>`
+        }
+        tag += `</td>`
+        return tag;
     }
 }
 
-const homeConfig = [
-    columnConfig("Rank", "rank"),
-    columnConfig(`<img class="duckIcon" src="/client/public/img/duckIcon.svg">`, "img"),
-    columnConfig("Number", "id"),
-    columnConfig("Version", "version"),
-    columnConfig("Parties", "parties"),
-    columnConfig("Rank Change", "rarityChange"),
-    columnConfig("Owner", "bludmoneyy", true),
-    columnConfig("Days Owned", "184", true),
-    columnConfig("Opensea", `<img src="/client/public/img/opensea.svg">`, true),
-    columnConfig("Looksrare", `<img src="/client/public/img/looksrare.svg">`, true),
-]
+// <th scope="col"><img class="duckIcon" src="/client/public/img/duckIcon.svg"></th>
+console.log(columnConfig().setHeaderStatic("Rank").setColumnKey("rank").getHeaderTag());
 
-const imageSuffixes = ['.png', '.jpg', '.jpeg', '.gif', '.svg'];
+function columnConfig() {
+    return new ColumnConfig()
+}
+
+const homeConfig = [
+    columnConfig().setHeaderStatic("Rank").setColumnKey("rank"),
+    columnConfig().setHeaderType('img').setHeaderClass("duckIcon").setHeaderSrc("/client/public/img/duckIcon.svg").setColumnKey("img").setColumnType('img').setColumnClass("duckIcon"),
+    columnConfig().setHeaderStatic("Number").setColumnKey("id"),
+    columnConfig().setHeaderStatic("Version").setColumnKey("version"),
+    columnConfig().setHeaderStatic("Parties").setColumnKey("parties"),
+    columnConfig().setHeaderStatic("Rank Change").setColumnKey("rarityChange"),
+    columnConfig().setHeaderStatic("Owner").setColumnStatic("bludmoneyy"),
+    columnConfig().setHeaderStatic("Days Owned").setColumnStatic("184"),
+    columnConfig().setHeaderStatic("Opensea").setColumnType('img').setColumnSrc("/client/public/img/opensea.svg").setColumnHref("https://opensea.io/collection/rubber-duck-bath-party"),
+    columnConfig().setHeaderStatic("Looksrare").setColumnType('img').setColumnSrc("/client/public/img/looksrare.svg").setColumnHref("https://looksrare.org/collections/0x7A4D1b54dD21ddE804c18B7a830B5Bc6e586a7F6")
+]
 
 function loadTableHeader(config) {
     const tableHeader = document.getElementById('tableHeader');
     let header = '<tr>';
     for (let column of config) {
-        header += `<th scope="col">${column.headerName}</th>`;
+        header += column.getHeaderTag();
     }
     header += '</tr>';
     tableHeader.innerHTML = header;
@@ -65,21 +156,7 @@ function loadTableData(duckData, start, stop, config) {
     for (let duck of ducks) {
         let duckRow = '<tr>';
         for (let column of config) {
-            if (column.override) {
-                if (column.key.includes(".img") || column.key.includes(".svg")) {
-                    duckRow += `<td><a href="https://opensea.io/collection/rubber-duck-bath-party" class="rowTitle">
-                                ${column.key}
-                                </a></td>`;
-                } else {
-                    console.log("rowOverride", column.override);
-                    duckRow += `<td><strong class="rowTitle">${column.key}</strong></td>`;
-                }
-
-            } else if (duck.hasOwnProperty(column.key) && imageSuffixes.some(suffix => String(duck[column.key]).endsWith(suffix))) {
-                duckRow += `<td><img class="duckIcon" src="${duck[column.key]}"></td>`;
-            } else {
-                duckRow += `<td><strong class="rowTitle">${duck[column.key]}</strong></td>`;
-            }
+            duckRow += column.getColumnTag(duck);
         }
         duckRow += '</tr>';
         duckTable += duckRow;
