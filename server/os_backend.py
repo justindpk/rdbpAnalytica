070030ea6650c01ca45002ea12d827b89fb68a28
cpiscos <christian.piscos@gmail.com>
2022-10-29 23:34:04 -0700

    Removed backend files and cleaned up folder structure.

--- server/os_backend.py
+++
@@ -1,6 +0,0 @@
-import requests
-def extract_source(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'}
-    source=requests.get(url, headers=headers).text
-    return source
-print(extract_source("https://opensea.io/assets/ethereum/0x7a4d1b54dd21dde804c18b7a830b5bc6e586a7f6/1"))
