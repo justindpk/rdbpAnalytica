231f9e08be6ac1fbd779eda1705e1d4ead32bf8c
Justin <jdapatrick@gmail.com>
2022-11-01 08:44:53 -0700

    img and a tag fixes

--- client/src/App.js
+++ client/src/App.js
@@ -86,39 +86,39 @@ function TopBar() {
         <div className="logoAndTitle">
 
           <a className="analyticaLogo" href="#top">
-            <img src="/img/analyticaStrawberryDuck.png"/>
+            <img src="/img/analyticaStrawberryDuck.png" alt="strawberry duck"/>
           </a>
           <div className="analyticaTitle">
-            <a> RDBP Analytica </a>
+            <a href="top"> RDBP Analytica </a>
             <p className="description">
-              A community project analyzing the <a className="description" href="https://duck.art/" target="_blank">
+              A community project analyzing the <a className="description" href="https://duck.art/" target="_blank" rel="noreferrer">
               Rubber Duck Bath Party</a> NFT collection.
             </p>
           </div>
         </div>
         <div className="links">
-          <a className="socialMediaSvg" href="https://duck.art/" target="_blank">
-            <img className="darkDuckIcon" src="/img/darkDuck.png"/>
+          <a className="socialMediaSvg" href="https://duck.art/" target="_blank" rel="noreferrer">
+            <img className="darkDuckIcon" src="/img/darkDuck.png" alt="duck.art" />
           </a>
-          <a className="socialMediaSvg" href="https://opensea.io/collection/rubber-duck-bath-party" target="_blank">
-            <img src="/img/opensea.svg"/>
+          <a className="socialMediaSvg" href="https://opensea.io/collection/rubber-duck-bath-party" target="_blank" rel="noreferrer">
+            <img src="/img/opensea.svg" alt="opensea"/>
           </a>
           <a className="socialMediaSvg"
-             href="https://looksrare.org/collections/0x7A4D1b54dD21ddE804c18B7a830B5Bc6e586a7F6" target="_blank">
-            <img src="/img/looksrare.svg"/>
+             href="https://looksrare.org/collections/0x7A4D1b54dD21ddE804c18B7a830B5Bc6e586a7F6" target="_blank" rel="noreferrer">
+            <img src="/img/looksrare.svg" alt="looksrare"/>
           </a>
-          <a className="socialMediaSvg" href="https://twitter.com/rubberduckbp" target="_blank">
-            <img src="/img/twitter.svg"/>
+          <a className="socialMediaSvg" href="https://twitter.com/rubberduckbp" target="_blank" rel="noreferrer">
+            <img src="/img/twitter.svg" alt="twitter" />
           </a>
-          <a className="socialMediaSvg" href="https://discord.gg/rdbp" target="_blank">
-            <img src="/img/discord.svg"/>
+          <a className="socialMediaSvg" href="https://discord.gg/rdbp" target="_blank" rel="noreferrer">
+            <img src="/img/discord.svg" alt="discord" />
           </a>
         </div>
       </header>
 
       <div className="doubleHeader">
         <div className="screenerTitle">
-          <a id="screenerTitle"> Duck Screener </a>
+          <a href="/" id="screenerTitle"> Duck Screener </a>
         </div>
       </div>
     </>
@@ -130,11 +130,11 @@ function TableTypeBar({setTableType}) {
   return (
     <div className="filterBar">
       <button className="button row yellow" onClick={() => setTableType("main")}>
-        <img className="duckButtonIcon" src="/img/duckIcon.svg"/>
+        <img className="duckButtonIcon" src="/img/duckIcon.svg" alt="ducks"/>
       </button>
       <button className="button row red" onClick={() => setTableType("traits")}>Traits</button>
       <button className="button row lightPurple" onClick={() => setTableType("backpacks")}>
-        <img className="backpackIcon" src="/img/backpack.png"/>
+        <img className="backpackIcon" src="/img/backpack.png" alt="backpacks"/>
       </button>
     </div>
   )
