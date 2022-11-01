68be4e410869b6b141fbca6ecc1a37017dcbe4c3
cpiscos <christian.piscos@gmail.com>
2022-10-31 21:22:28 -0700

    Added table type buttons

--- client/src/App.js
+++ client/src/App.js
@@ -111,33 +111,54 @@ function App() {
         <div className="logoAndTitle">
 
           <a className="analyticaLogo" href="#top">
-            <img src="/img/analyticaStrawberryDuck.png" />
+            <img src="/img/analyticaStrawberryDuck.png"/>
           </a>
           <div className="analyticaTitle">
             <a> RDBP Analytica </a>
-            <p className="description"> A community project analyzing the <a className="description" href="https://duck.art/" target="_blank">Rubber Duck Bath Party</a> NFT collection.</p>
+            <p className="description"> A community project analyzing the <a className="description"
+                                                                             href="https://duck.art/" target="_blank">Rubber
+              Duck Bath Party</a> NFT collection.</p>
           </div>
         </div>
-        {/*<div className="links">*/}
-        {/*  <a className="socialMediaSvg" href="https://duck.art/" target="_blank"><img className="darkDuckIcon" src="/client1/public/img/darkDuck.png"></a>*/}
-        {/*  <a className="socialMediaSvg" href="https://opensea.io/collection/rubber-duck-bath-party" target="_blank"><img src="/client1/public/img/opensea.svg"></a>*/}
-        {/*  <a className="socialMediaSvg" href="https://looksrare.org/collections/0x7A4D1b54dD21ddE804c18B7a830B5Bc6e586a7F6" target="_blank"><img src="/client1/public/img/looksrare.svg"></a>*/}
-        {/*  <a className="socialMediaSvg" href="https://twitter.com/rubberduckbp" target="_blank"><img src="/client1/public/img/twitter.svg"></a>*/}
-        {/*  <a className="socialMediaSvg" href="https://discord.gg/rdbp" target="_blank"><img src="/client1/public/img/discord.svg"></a>*/}
-        {/*</div>*/}
+        <div className="links">
+          <a className="socialMediaSvg" href="https://duck.art/" target="_blank"><img className="darkDuckIcon"
+                                                                                      src="/img/darkDuck.png"/></a>
+          <a className="socialMediaSvg" href="https://opensea.io/collection/rubber-duck-bath-party" target="_blank"><img
+            src="/img/opensea.svg"/></a>
+          <a className="socialMediaSvg"
+             href="https://looksrare.org/collections/0x7A4D1b54dD21ddE804c18B7a830B5Bc6e586a7F6" target="_blank"><img
+            src="/img/looksrare.svg"/></a>
+          <a className="socialMediaSvg" href="https://twitter.com/rubberduckbp" target="_blank"><img
+            src="/img/twitter.svg"/></a>
+          <a className="socialMediaSvg" href="https://discord.gg/rdbp" target="_blank"><img src="/img/discord.svg"/></a>
+        </div>
       </header>
+
       <div className="doubleHeader">
         <div className="screenerTitle">
-          {loaded ? (
-            <div>
-              <p className="screenerTitle">Loaded:
-                {Object.keys(databases).map((databaseName) => " " + databaseName)}</p>
-              <Table databases={databases}/>
-            </div>
-          ) : (
-            <p className="screenerTitle">Loading...</p>
-          )}
+          <a id="screenerTitle"> Duck Screener </a>
         </div>
+      </div>
+
+      <div className="filterBar">
+        <button className="button row yellow" onClick="generateDuckTable()"><img className="duckButtonIcon"
+                                                                                 src="/img/duckIcon.svg"/></button>
+        <button className="button row red" onClick="generateTraitTable()">Traits</button>
+        <button className="button row lightPurple" onClick="generateBackpackTable()"><img className="backpackIcon"
+                                                                                          src="/img/backpack.png"/>
+        </button>
+      </div>
+
+      <div className="screenerTitle">
+        {loaded ? (
+          <div>
+            <p className="screenerTitle">Loaded:
+              {Object.keys(databases).map((databaseName) => " " + databaseName)}</p>
+            <Table databases={databases}/>
+          </div>
+        ) : (
+          <p className="screenerTitle">Loading...</p>
+        )}
       </div>
     </div>
   );
