b8a5213622ab5ebf0a90a5b24b238eb2a8bad0ef
cpiscos <christian.piscos@gmail.com>
2022-11-02 19:09:00 -0700

    Flip arrow on reverse sort.

--- client/src/components/MainTable.js
+++ client/src/components/MainTable.js
@@ -20,21 +20,31 @@ function MainTable({databases, setDatabases, amountToLoad}) {
     setSortBy(name);
   }
 
+  function handleSortClassName(name) {
+    if (name === sortBy) {
+      return "selected"
+    } else {
+      return "";
+    }
+  }
+
+
   return (
     <table>
       <thead>
       <tr>
-        <th onClick={() => handleSort('rank', (a, b) => a.history[0].rank - b.history[0].rank)}>
-          Rank ▼</th>
+        <th onClick={() => handleSort('rank', (a, b) => a.history[0].rank - b.history[0].rank)}
+            className={handleSortClassName('rank')}>
+          Rank {'rank' === sortBy && !sortAscending ? '▲' : '▼'}</th>
         <th><img src="/img/duckIcon.svg" alt="duck" className='duckIcon'/></th>
-        <th onClick={() => handleSort('id', (a, b) => a.duck - b.duck)}>ID ▼</th>
-        <th onClick={() => handleSort('version', (a, b) => a.history[0].version - b.history[0].version)}>
-          Version ▼</th>
-        <th onClick={() => handleSort('parties', (a, b) => a.attributes[0].value - b.attributes[0].value)}>
-          Parties ▼</th>
-        <th onClick={() => handleSort('rankChange', (a, b) =>
+        <th className={handleSortClassName('id')} onClick={() => handleSort('id', (a, b) => a.duck - b.duck)}>ID {'id' === sortBy && !sortAscending ? '▲' : '▼'}</th>
+        <th className={handleSortClassName('version')} onClick={() => handleSort('version', (a, b) => a.history[0].version - b.history[0].version)}>
+          Version {'version' === sortBy && !sortAscending ? '▲' : '▼'}</th>
+        <th className={handleSortClassName('parties')} onClick={() => handleSort('parties', (a, b) => a.attributes[0].value - b.attributes[0].value)}>
+          Parties {'parties' === sortBy && !sortAscending ? '▲' : '▼'}</th>
+        <th className={handleSortClassName('rankChange')} onClick={() => handleSort('rankChange', (a, b) =>
           (a.history.length > 1 ? a.history[1].rank - a.history[0].rank : 0) -
-          (b.history.length > 1 ? b.history[1].rank - b.history[0].rank : 0))}>Rank Change ▼
+          (b.history.length > 1 ? b.history[1].rank - b.history[0].rank : 0))}>Rank Change {'rankChange' === sortBy && !sortAscending ? '▲' : '▼'}
         </th>
         <th>Opensea</th>
       </tr>
@@ -43,14 +53,14 @@ function MainTable({databases, setDatabases, amountToLoad}) {
       {databases['allDucks'].slice(0, amountToLoad).map((duck, index) => {
         return (
           <tr key={index}>
-            <td>{duck.history[0].rank}</td>
+            <td className={handleSortClassName('rank')}>{duck.history[0].rank}</td>
             <td><a href={`https://duck.art/${duck.duck}`} target="_blank" rel="noreferrer">
               <img src={duck.history[0].image} alt="duck" className='duckImage'/></a>
             </td>
-            <td>{duck.duck}</td>
-            <td>{duck.history[0].version}</td>
-            <td>{duck.attributes[0].value} / {duck.attributes[0].max_value}</td>
-            <td>{duck.history.length > 1 ? duck.history[1].rank - duck.history[0].rank : 0}</td>
+            <td className={handleSortClassName('id')}>{duck.duck}</td>
+            <td className={handleSortClassName('version')}>{duck.history[0].version}</td>
+            <td className={handleSortClassName('parties')}>{duck.attributes[0].value} / {duck.attributes[0].max_value}</td>
+            <td className={handleSortClassName('rankChange')}>{duck.history.length > 1 ? duck.history[1].rank - duck.history[0].rank : 0}</td>
             <td><a href={`https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/${duck.duck}`}
                    target="_blank" rel="noreferrer"><img src={"/img/opensea.svg"} alt="opensea"/></a></td>
           </tr>
